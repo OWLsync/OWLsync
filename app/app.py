4e085cdbc748b8eea7b32b55c8eb1a111bf4dabf
Peter Reijnders <peter.reijnders@verpeteren.nl>
2018-01-15 20:21:35 +0100

    global variables in uppercase

--- app/app.py
+++ app/app.py
@@ -11,44 +11,44 @@ from wtforms.validators import DataRequired
 
 from config import Configuration  # import our configuration data.
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-app.config.from_object(Configuration)  # use values from our
+APP.config.from_object(Configuration)  # use values from our
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP.config['SQLALCHEMY_ECHO'] = True
 
-db = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
 # ____________________POSTGRES CONNECTION
 
-DB_url = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=app.config["POSTGRES_USER"],
-    pw=app.config["POSTGRES_PW"], url=app.config["POSTGRES_URL"], db=app.config["POSTGRES_DB"])
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=APP.config["POSTGRES_USER"],
+    pw=APP.config["POSTGRES_PW"], url=APP.config["POSTGRES_URL"], db=APP.config["POSTGRES_DB"])
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_url
+APP.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 
 # _________________________________________
-engine = create_engine(DB_url, convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+ENGINE = create_engine(DB_URL, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=ENGINE))
 
-Base = declarative_base()
-Base.query = db_session.query_property()
+BASE = declarative_base()
+BASE.query = db_session.query_property()
 
-session_factory = sessionmaker(bind=engine)
+session_factory = sessionmaker(bind=ENGINE)
 Session = scoped_session(session_factory)
 # ________________________________________
 
-SECRET_KEY = app.config["SECRET_KEY"]
-DOMAIN_SERVER = app.config["DOMAIN_SERVER"]
+SECRET_KEY = APP.config["SECRET_KEY"]
+DOMAIN_SERVER = APP.config["DOMAIN_SERVER"]
 
 
 # DB MODELS
-class User(Base):
+class User(BASE):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    user_first_name = db.Column(db.String(35), unique=False, nullable=False)
-    user_last_name = db.Column(db.String(35), unique=False, nullable=False)
-    posts = db.Column(db.String(2000), unique=False, nullable=False)
+    id = DB.Column(DB.Integer, primary_key=True)
+    user_first_name = DB.Column(DB.String(35), unique=False, nullable=False)
+    user_last_name = DB.Column(DB.String(35), unique=False, nullable=False)
+    posts = DB.Column(DB.String(2000), unique=False, nullable=False)
 
 
 # xu = User.query.order_by('-id').first()
@@ -62,42 +62,41 @@ class Userform(FlaskForm):
     submit = SubmitField("Submit")
 
 
-@app.route('/userregister', methods=('GET', 'POST'))
+@APP.route('/userregister', methods=('GET', 'POST'))
 def userregister():
     form = Userform()
     if form.validate_on_submit():
         try:
-            db.create_all()
+            DB.create_all()
             user_data = User()
             user_data.user_first_name = form.user_first_name.data
             user_data.user_last_name = form.user_last_name.data
             user_data.posts = form.posts.data
-            db.session.add(user_data)
-            db.session.commit()  # calls flush beforehand, but we need it after the commit
+            DB.session.add(user_data)
+            DB.session.commit()  # calls flush beforehand, but we need it after the commit
             print("{0}  {1}  {2}".format(user_data.user_first_name, user_data.user_last_name, user_data.posts))
-            db.session.flush()  # updates the objects of the session
+            DB.session.flush()  # updates the objects of the session
         except Exception as e:
-            db.session.rollback()
+            DB.session.rollback()
             print(e)
     return render_template('userregister.html', form=form)
 
 
 # VIEWS
-@app.route("/")
-@app.route("/index")
+@APP.route("/")
+@APP.route("/index")
 def index():
     try:
-        Users = db.session.query(User).all()
+        Users = DB.session.query(User).all()
         return render_template("index.html", Users=Users)
     except Exception as e:
         return str(e)
 
-
-@app.route('/posts')
+@APP.route('/posts')
 def view_posts():
-    Users = db.session.query(User).all()
+    Users = DB.session.query(User).all()
     return render_template("posts.html", Users=Users), 200
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=False, debug=True)
+    APP.run(use_reloader=False, debug=True)
